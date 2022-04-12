c4c3802ec1236bcbe26ca937a3bc75dbe8c7534e
SaurabhAgarwal207 <mcasaurabhagarwal@gmail.com>
2022-04-12 08:36:50 +0530

    Sum of 2 numbers

---
+++ src/com/makeyoupro/basics/SumOfTwoNumbers.java
@@ -0,0 +1,9 @@
+package com.makeyoupro.basics;
+//Addition of 2 numbers
+public class SumOfTwoNumbers {
+    public static void main(String[] args) {
+        int a =5;
+        int b=10;
+        System.out.println("Sum of "+a+" and "+b+" is :"+(a+b));
+    }
+}
